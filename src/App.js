c68665e0971e9ce858205b0996b8e8a9cf2dfb6f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-26 23:28:30 +0100

    corrected typos on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Grettings from './components/Greetings';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<h2 className="centralize">Welcome to my greeting app</h2>} />
-        <Route path="greetings" element={<Grettings />} />
+        <Route path="greetings" element={<Greetings />} />
       </Routes>
     </Router>
   );
