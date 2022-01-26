9efde5e59920a5ab6eb443e47459cfbc0f072adf
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-26 13:31:17 +0100

    create store, reducers and actions

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import greetingReducer from './greetings/greetings'
+
+const reducer = combineReducers({
+  greeting: greetingReducer,
+})
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+export default store;
